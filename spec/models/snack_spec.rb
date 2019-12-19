6776994515342dbb6d530d34a71b28ac5794963c
Becky Robran <rerobran@gmail.com>
2019-12-19 09:42:11 -0700

    Adds testing for model method

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Snack, type: :model do
   describe "relationships" do
     it {should belong_to :machine}
   end
+
+  describe "methods" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    burger = dons.snacks.create(name: "White Castle Burger", price: 3_50)
+    pop_rocks = dons.snacks.create(name: "Pop Rocks", price: 1_50)
+    cheetos = dons.snacks.create(name: "Flaming Hot Cheetos", price: 2_50)
+
+    expect(Snack.ave_price).to eq(2_5)
+  end
 end
